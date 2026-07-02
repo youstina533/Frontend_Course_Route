6409e809094cf6442cf3f17e3c15317762ba1bd6
Youstina Abdelmeseh <youstinaabdelmeseh@yahoo.com>
2026-07-02 19:04:59 +0300

    Add files via upload

---
+++ Hoisting Questions.js
@@ -0,0 +1,78 @@
+/*
+
+function foo() {
+  function bar() { return 3  }
+
+  return bar()
+  function bar() {return 8 }
+}
+alert(foo())
+
+*/
+
+
+
+
+
+
+
+/*
+
+function foo() {
+  var bar = function() { return 3  }
+
+  return bar()
+  var bar = function() { return 8  }
+}
+alert(foo())
+
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+function foo() {
+  return bar()
+
+  function bar() { return 3  }
+
+  var bar = function() {return 8 }
+}
+alert(foo())
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+console.log(foo());
+var foo = function() {
+  return bar()
+
+  function bar() { return 3  }
+
+  var bar = function() {return 8 }
+}
+
+*/
