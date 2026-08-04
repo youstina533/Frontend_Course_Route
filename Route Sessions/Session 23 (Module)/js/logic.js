56319409eea9fbdb741fb7bda275997bee568349
Youstina Abdelmeseh <youstinaabdelmeseh@yahoo.com>
2026-08-04 21:37:05 +0300

    Add files via upload

---
+++ Route Sessions/Session 23 (Module)/js/logic.js
@@ -0,0 +1,17 @@
+function showData(){
+  console.log("DATA");
+}
+
+function displayData(){
+  console.log("DATA2");
+}
+
+function getData(){
+  console.log("get data");
+}
+
+export {
+  showData,
+  displayData
+}
+export default getData;
---
+++ Route Sessions/Session 23 (Module)/js/main.js
@@ -0,0 +1,5 @@
+import * as dataCode from "./logic.js";
+import getData from "./logic.js";
+dataCode.showData();
+getData();
+dataCode.displayData();
