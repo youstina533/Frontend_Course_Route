50533678df3990b06438caba6418d96c2f7698ab
Youstina Abdelmeseh <youstinaabdelmeseh@yahoo.com>
2026-08-11 18:21:32 +0300

    Add files via upload

---
+++ Route Sessions/Session 24 (Typescript)/js/index.js
@@ -0,0 +1,9 @@
+let x = "female";
+console.log(x);
+let ahmed = {
+    name: "Ahmed",
+    email: "ahmed@yahoo.com",
+    age: 25
+};
+console.log(ahmed);
+export {};
---
+++ Route Sessions/Session 24 (Typescript)/js/index.ts
@@ -0,0 +1,17 @@
+export {} //Lazm 34an a3ml scope mo5tlf 3n index.js
+type gender = "male" | "female"
+let x:gender = "female"
+console.log(x);
+
+interface User{
+    name : string,
+    email : string,
+    age : number
+}
+
+let ahmed : User = {
+    name: "Ahmed",
+    email : "ahmed@yahoo.com",
+    age: 25
+}
+console.log(ahmed);
